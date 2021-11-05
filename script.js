d3886746944eb5e46ab1c9905122dafa797f62b3
shckqad <87878268+shckqad@users.noreply.github.com>
2021-11-05 17:36:47 -0400

    Assignment 2
    Assignment 2

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 var vaccinated = true;
 var name = 'Michael';
-var message = 'Hi '+ name +' your vaccination has been approved you may enter';
+var message = 'Hi '+ name +' your vaccination form has been recieved ';
 
 var element = document.getElementById('corncurls');
 
 element.textContent = message;
 
-console.log(message);
+var vCard = {
+name: 'Michael Reid',
+vaccinationType: 'Pfizer',
+sex: 'M',
+Age: '27',
+fullVacc: true,
+dose2: new Date('Oct 31, 2021 10:00:00'),
+ getDifference: function(){
+ var today = new Date()
+ var difference = today.getTime() - vCard.dose2.getTime();
+ difference = Math.floor(difference/84600000);
+ return difference;
+ }
+};
+
+var date = vCard.getDifference();
+var daysUntil = 14 - date;
+
+
+if (vCard.fullVacc == true) {
+ if (date >= 14) {
+ var message = document.getElementById("message");
+ message.textContent= "You're Vaccine has been Approved";
+ } else {
+   var message = document.getElementById("message");
+    message.textContent= "You've got " + daysUntil + " days remaining before being fully vaccinated.";
+ }
+ } else { var message = document.getElementById("message");
+    message.textContent= "You haven't been vaccinated. Entry Denied."
+}
